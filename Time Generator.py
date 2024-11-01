bc65529f2fcd953b11b8e3a3db44ee64f48b995d
Ad-closeNN <1709301095@qq.com>
2024-11-01 22:49:18 +0800

    Try

--- Time Generator.py
+++ Time Generator.py
@@ -7,11 +7,14 @@ while a<=100:
     beijing_time = utc_time.astimezone(beijing_tz)
     with open("README.md", "w", encoding="utf-8") as f:
         f.write("# " + beijing_time.strftime('%Y-%m-%d %H:%M:%S'))
+        f.write("\n\n")
+        url = """<a href="https://github.com/Ad-closeNN"><img align="center" src="https://ad-closenn-stats.vercel.app/api?username=Ad-closeNN&show_icons=true&include_all_commits=true&theme=buefy" alt="Ad-closeNN's github stats" /></a>"""
+        f.write(url)
     os.system("git add .")
     os.system("git commit -S -m auto-run")
     os.system("git pull --rebase")
     os.system("git push")
     print("本次为第",a,"次")
     a = a + 1
-    time.sleep(0.0001)
+    time.sleep(3)
 print("已成功执行 100 次")
