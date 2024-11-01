e3889451f7a7fb34a685ac58400e9f5b41446dab
Ad-closeNN <1709301095@qq.com>
2024-11-01 23:08:03 +0800

    上强度

--- Time Generator.py
+++ Time Generator.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import pytz, os, time
 a = 0
-while a<=100:
+while a<=10000:
     utc_time = datetime.now(pytz.utc)
     beijing_tz = pytz.timezone('Asia/Shanghai')
     beijing_time = utc_time.astimezone(beijing_tz)
@@ -16,4 +16,4 @@ while a<=100:
     print("本次为第",a,"次")
     a = a + 1
     time.sleep(1)
-print("已成功执行 100 次")
+print("已成功执行 10000 次")
