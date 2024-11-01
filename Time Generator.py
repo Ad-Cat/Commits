87105f7cadef19de091f5bd0a8fd4fce3c548a11
Ad-closeNN <1709301095@qq.com>
2024-11-01 22:57:44 +0800

    10000

--- Time Generator.py
+++ Time Generator.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import pytz, os, time
 a = 0
-while a<=100:
+while a<=10000:
     utc_time = datetime.now(pytz.utc)
     beijing_tz = pytz.timezone('Asia/Shanghai')
     beijing_time = utc_time.astimezone(beijing_tz)
@@ -15,4 +15,4 @@ while a<=100:
     os.system("git push")
     print("本次为第",a,"次")
     a = a + 1
-print("已成功执行 100 次")
+print("已成功执行 10000 次")
